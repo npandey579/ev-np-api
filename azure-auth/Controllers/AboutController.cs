71d556634264006ca33eb06bf1da5a6bf3a2b280
Nitesh Pandey <npandey579@live.com>
2020-09-06 22:10:00 +0530

    adding about controoler

---
+++ azure-auth/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace azureauth.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        [HttpGet]
+        public ContentResult About()
+        {
+            return Content("An API listing events");
+        }
+    }
+}
